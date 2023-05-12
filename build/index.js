5f171c6f0f810655da6e4d7a97dad4fb9f347104
jasonmccormack1970 <jason.mccormack70@gmail.com>
2023-05-12 01:18:04 +0100

    higher order functions

--- build/index.js
+++ build/index.js
@@ -8,12 +8,11 @@ const workCentre1 = new classes_1.Work(mach1, opr1);
 console.clear();
 console.table(workCentre1);
 console.log(opr1.name, workCentre1.isOK());
-const nums = [];
-// create a function to generate a Fibonacci sequence up to 100.
-function fibonacci(n) {
-    if (n <= 1)
-        return 1;
-    return fibonacci(n - 1) + fibonacci(n - 2);
-}
-const fibonacciSequence = nums.map(fibonacci(100));
-console.log(fibonacciSequence);
+// const nums= []
+// // create a function to generate a Fibonacci sequence up to 100.
+// function fibonacci(n) {  
+//       if (n <= 1) return 1;
+//     return fibonacci(n - 1) + fibonacci(n - 2);
+//   }  
+// const fibonacciSequence = nums.map(fibonacci(100))
+// console.log(fibonacciSequence)
--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,3 @@ console.table(workCentre1)
 console.log(opr1.name, workCentre1.isOK())
 
 
-// const nums= []
-
-// // create a function to generate a Fibonacci sequence up to 100.
-// function fibonacci(n) {  
-//       if (n <= 1) return 1;
-//     return fibonacci(n - 1) + fibonacci(n - 2);
-//   }  
-
-// const fibonacciSequence = nums.map(fibonacci(100))
-// console.log(fibonacciSequence)
